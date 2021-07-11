e577a39b8024fad4612ec66387f9e45340c046f3
njrom <njrom@hey.com>
2021-07-11 16:45:13 -0400

    Removes tests

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "MEPCore",
             dependencies: []),
-        .testTarget(
-            name: "MEPCoreTests",
-            dependencies: ["MEPCore"]),
     ]
 )
