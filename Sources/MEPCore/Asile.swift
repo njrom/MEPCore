ed5d9b2696155adb8be5c0017058ec9a48e3d227
njrom <njrom@hey.com>
2021-07-22 14:25:22 -0400

    Adds new aisles to enum

--- Sources/MEPCore/Asile.swift
+++ Sources/MEPCore/Asile.swift
@@ -8,9 +8,19 @@
 import Foundation
 
 public enum Asile: String, CaseIterable, Codable {
-    case frozenFood = "Frozen Food"
-    case meat = "Meat"
-    case cereals = "Cereals"
+    case baking = "Baking"
+    case beerAndWine = "Beer and Wine"
+    case beverages = "Beverages"
+    case bakery = "Bread / Bakery"
     case cannedGoods = "Canned Goods"
+    case cereals = "Cereals"
+    case condiments = "Condiments"
+    case deli = "Deli / Prepared Foods"
+    case dairy = "Dairy"
+    case heathAndBeauty = "Heath and Beauty"
     case produce = "Produce"
+    case meat = "Meat"
+    case misc = "Miscellaneous"
+    case snackFoods = "Snack Foods"
+    case frozenFood = "Frozen Food"
 }
